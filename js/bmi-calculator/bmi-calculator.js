59fa9a5dfccb946addf4bb004b40ee1c15dd40f2
Max Samsonenko <samsonenko.maxwork@gmail.com>
2023-10-08 19:13:36 +0300

    bmi calculator

---
+++ js/bmi-calculator/bmi-calculator.js
@@ -0,0 +1,73 @@
+import { explanationText, importantText } from "./bmi-explanation.js";
+
+const form = document.querySelector("#bmi-form");
+const weight = document.querySelector("#weight");
+const height = document.querySelector("#height");
+const result = document.querySelector(".result");
+const resultText = document.querySelector(".bmi-explanation");
+
+form.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(e) {
+	e.preventDefault();
+
+	if (Number(weight.value) > 500 || Number(height.value) >= 300) {
+		alert("Really, dude?!");
+		inputFieldClear();
+		return;
+	}
+	const bmI = calculateBMI(Number(weight.value), Number(height.value)).toFixed(
+		1
+	);
+	result.innerHTML = bmI;
+	addExplanation(userCategory(bmI));
+
+	inputFieldClear();
+}
+
+//calculate BMI
+function calculateBMI(weight, height) {
+	let heightInMeters = height / 100;
+	return weight / (heightInMeters * heightInMeters);
+}
+
+//clear input field after form submit
+function inputFieldClear() {
+	weight.value = "";
+	height.value = "";
+}
+//check which category falls under the resulted bmi
+function userCategory(bmi) {
+	let weightType = null;
+	if (bmi < 18.5) {
+		weightType = "underweight";
+	} else if (bmi > 18.5 && bmi < 24.9) {
+		weightType = "normal";
+	} else if (bmi > 25 && bmi < 29.9) {
+		weightType = "overweight";
+	} else if (bmi > 30 && bmi < 34.9) {
+		weightType = "obesity1";
+	} else if (bmi > 35 && bmi < 39.9) {
+		weightType = "obesity2";
+	} else if (bmi > 40) {
+		weightType = "obesity3";
+	} else {
+		return alert("You've entered incorrect parameters");
+	}
+	return weightType;
+}
+//create explanation text markup according to results
+function createMarkup(arr) {
+	return arr.map((item) => `<li>${item}</li>`).join("");
+}
+//Add text according to the result
+function addExplanation(weightType) {
+	const [{ eng }] = explanationText;
+	const { [weightType]: categoryInfo } = eng;
+	const { explain, recommend } = categoryInfo;
+	const markup = `<p class="exp-text">Your result: <strong>${weightType}</strong></p>
+			<span class="explain">${explain}</span>
+			<ul class="rcmd-list">${createMarkup(recommend)}</ul>
+			<p class="important-note"><strong>${importantText}</strong></p>`;
+	resultText.innerHTML = markup;
+}
---
+++ js/bmi-calculator/bmi-explanation.js
@@ -0,0 +1,64 @@
+export const explanationText = [
+	{
+		eng: {
+			underweight: {
+				explain:
+					"Individuals in this category have a BMI below the normal weight range. This may indicate insufficient body fat and could be associated with health risks.",
+				recommend: [
+					"Consult a healthcare professional for a comprehensive evaluation to identify potential underlying causes.",
+					"Focus on a balanced diet with adequate calorie intake to promote healthy weight gain.",
+					"Incorporate strength-training exercises to build muscle mass.",
+					"Seek guidance from a registered dietitian or nutritionist for a tailored meal plan.",
+				],
+			},
+			normal: {
+				explain:
+					"Individuals in this category have a BMI within the healthy weight range. This is associated with a lower risk of weight-related health issues.",
+				recommend: [
+					"Maintain a balanced diet with a variety of nutrients.",
+					"Engage in regular physical activity to promote overall health and fitness.",
+					"Focus on maintaining a healthy lifestyle, including stress management and adequate sleep.",
+				],
+			},
+			overweight: {
+				explain:
+					"Individuals in this category have a BMI indicating excess body weight, which can increase the risk of certain health problems.",
+				recommend: [
+					"Consider making dietary changes, such as reducing calorie intake and choosing healthier foods.",
+					"Increase physical activity to help with weight loss.",
+					"Consult with a healthcare provider or registered dietitian for personalized weight management strategies.",
+				],
+			},
+			obesity1: {
+				explain:
+					"This is the first class of obesity. Individuals in this category have a higher level of excess body fat, which can increase the risk of various health conditions.",
+				recommend: [
+					"Work with a healthcare provider or registered dietitian to develop a weight loss plan.",
+					"Focus on both diet and exercise for weight management.",
+					"Monitor health parameters and consider medical evaluations as needed.",
+				],
+			},
+			obesity2: {
+				explain:
+					"This is the second class of obesity. Individuals in this category have a higher degree of excess body fat and face an increased risk of obesity-related health issues.",
+				recommend: [
+					"Seek professional medical guidance and monitoring.",
+					"Consider more structured weight loss programs or surgical options, depending on individual circumstances.",
+					"Prioritize lifestyle changes to improve overall health.",
+				],
+			},
+			obesity3: {
+				explain:
+					"This is the third class of obesity, often referred to as severe or morbid obesity. Individuals in this category face a significantly higher risk of obesity-related health complications.",
+				recommend: [
+					"Consult with a healthcare specialist experienced in managing severe obesity.",
+					"Consider bariatric surgery as a potential treatment option, under medical guidance.",
+					"Implement significant lifestyle changes and adhere to a comprehensive weight management plan.",
+				],
+			},
+		},
+	},
+];
+
+export const importantText =
+	"It's important to remember that BMI is a screening tool and does not provide a complete picture of an individual's health. Other factors, such as body composition, genetics, and overall lifestyle, also play a crucial role. Therefore, personalized recommendations should be based on a comprehensive assessment by a healthcare professional. Additionally, weight should not be the sole focus; overall health and well-being should be the primary goals.";
